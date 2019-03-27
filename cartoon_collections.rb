f4eb811869446afa241e1b8004333fb5d0580645
Daniel Bovington <d.bovington@gmail.com>
2019-03-27 18:22:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,9 @@
 
 #def summon_captain_planet(planeteer_calls)
 #  planeteer_calls.map |planeteer_calls| planeteer_calls.capitalize(!)
-end
+#end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls()
   # Your code here
 end
 
