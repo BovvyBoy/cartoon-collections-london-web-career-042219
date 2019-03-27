0620c9dacfa2282406e68b9ec1c1050bc4914380
Daniel Bovington <d.bovington@gmail.com>
2019-03-27 18:09:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
+#def roll_call_dwarves(dwarves)
+#  dwarves.each_index do |value, index|
+#    puts "#{value}: #{index}"
+#end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)
   # Your code here
 end
 
